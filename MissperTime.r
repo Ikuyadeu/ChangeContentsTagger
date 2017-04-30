1f25ce8effe8ecff31fee9e7243d10cc7a14d941
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2017-04-30 16:59:05 +0900

    Remake MissperTime.r

--- MissperTime.r
+++ MissperTime.r
@@ -1,24 +1,32 @@
-diffs <- read.csv("./diff.csv", sep = ',', header = TRUE, row.names = NULL)
-diffs2 <- read.csv("./date.csv", sep = ',', header = TRUE, row.names = NULL)
+library(xts)
+diffs <- read.csv("./diff2.csv", sep = ',', header = TRUE, row.names = NULL)
 
-thresholds <- c(0, 50, 100)
-cols <- c("#FF0000", "#00FF00", "0000FF")
+cols <- c("#FF0000", "#00FF00", "#0000FF",
+          "#FFFF00", "#FF00FF", "#00FFFF", "#888888")
 
 per.contents.denominator <- ""
 per.contents.numerators <- c("")
-diffs$Date <- as.Date(diffs2$Date)
+diffs$Date <- as.Date(diffs$Date)
 diffs <- subset(diffs, CHANGED_LINES > 0)
 
 per.lines.denominator <- "ChangeLines"
-# diffs$per.lines.numerators <- (diffs, NewLine + If + Comment + L_SpaceOrTab + L_UpperOrLower + L_Symbol + L_Renamed)
+numerator.names <- c("NewLine", "If", "Comment", "L_SpaceOrTab", "L_UpperOrLower", "L_Symbol", "L_Renamed")
+
+for (i in 1:length(numerator.names)) {
+    numerator.name <- numerator.names[i]
+    diffs[numerator.name] <- diffs[numerator.name] / diffs$CHANGED_LINES
+    bts <- apply.weekly(xts(diffs[numerator.name], diffs$Date), median)
+    if (i==1){
+        ts <- bts
+    } else {
+        ts <- na.locf(merge(ts, bts))
+    }
+}
 diffs$per.lines.numerators <- diffs$NewLine + diffs$If + diffs$Comment + diffs$L_SpaceOrTab + diffs$L_UpperOrLower + diffs$L_Symbol + diffs$L_Renamed
-diffs$per.lines.numerators <- diffs$per.lines.numerators / diffs$CHANGED_LINES
+bts <- apply.weekly(xts(diffs$per.lines.numerators, diffs$Date), median)
+ts <- na.locf(merge(ts, bts))
+numerator.names <- append(numerator.names, "ALL")
+cols <- append(cols, "#000000")
 
-write.csv(diffs, "./diff2.csv", row.names = TRUE, quote=TRUE)
-
-# diffs$isFew <- diffs$per.lines.numerators / diffs$ChangeLines
-# print(isFew)
-
-# ts <- apply.weekly(xts(diffs$SubDate, diffs$date), median)
-# plot.zoo(ts, ylim = c(0, 600), col = cols, plot.type = "single", ylab = "Few Change Num", xlab = "Date")
-# legend("topleft", legend = thresholds, fill = cols, cex = 1.0)
+plot.zoo(ts, ylim = c(0, 0.3), col = cols, plot.type = "single", ylab = "Small Change Per", xlab = "Date")
+legend("topleft", legend = numerator.names, fill = cols, cex = 1.0)
