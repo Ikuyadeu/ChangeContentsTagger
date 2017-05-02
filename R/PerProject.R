ce3cbbc3ae6c59d803d1247726b4425be013ea83
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-05-02 20:59:27 +0900

    make top3 project

---
+++ R/PerProject.R
@@ -0,0 +1,13 @@
+csvname <- "devs22.csv"
+diffs <- read.csv(paste("csv/", csvname, sep = ""), sep = ',', header = TRUE, row.names = NULL)
+
+projects <- c("qt/qtbase", "qt-creator/qt-creator", "qt/qtdeclarative")
+
+for (i in 1:length(projects)) {
+  proj <- projects[i]
+  newdiffs <- subset(diffs, diffs$project == proj)
+  proj <- gsub("/", "-", proj)
+  csvname2 <- paste("csv/QT", proj, "-",csvname, sep = "")
+  write.csv(newdiffs, csvname2, quote=TRUE, row.names=FALSE)  
+}
+
