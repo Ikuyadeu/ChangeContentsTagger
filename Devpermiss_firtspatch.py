f35c21039defe77dad970aef5b081fa7addd8395
YukiUeda <ikuyadeu@yahoo.co.jp>
2017-05-07 15:52:27 +0900

    little fix

--- Devpermiss_firtspatch.py
+++ Devpermiss_firtspatch.py
@@ -12,12 +12,12 @@ fieldnames = ["PullNo","patchNo","Date","CHANGED_CONTENTS","SpaceOrTab",
               "L_SpaceOrTab","L_UpperOrLower","L_Symbol","L_Renamed","L_OtherPer","OtherPer","devId", "exp"]
 
 fieldnames = ["PullNo", "exp", "devId", "Date"]
-# fieldnames = ["PullNo", "Date"]
+fieldnames = ["PullNo", "Date"]
 fieldnames.extend(kinds)
 
 with open('csv/newdiffs.csv', "r") as diff_file:
     pulls = csv.DictReader(diff_file)
-    with open('csv/devs2f.csv', "wb") as devfile:
+    with open('csv/devs3f.csv', "wb") as devfile:
         developers = {}
         writer = csv.DictWriter(devfile, fieldnames)
         writer.writeheader()
@@ -28,8 +28,8 @@ with open('csv/newdiffs.csv', "r") as diff_file:
             patchNo =int(pull["PatchNo"]) 
             if  patchNo == 1:
                 if i > 1:
-                    outpull.update(developers[dev_id])
-                    # outpull.update(kinds_flag)
+                    # outpull.update(developers[dev_id])
+                    outpull.update(kinds_flag)
                     writer.writerow(outpull)
 
                 dev_id = pull["devId"]
@@ -42,7 +42,9 @@ with open('csv/newdiffs.csv', "r") as diff_file:
                     for kind in kinds:
                         if float(pull[kind]) > 0:
                             developers[dev_id][kind] += 1
-                        kinds_flag[kind] = 0
+                            kinds_flag[kind] = 1
+                        else:
+                            kinds_flag[kind] = 0 
                 else:
                     developers[dev_id] = {}
                     developers[dev_id]["devId"] = dev_id
@@ -50,8 +52,9 @@ with open('csv/newdiffs.csv', "r") as diff_file:
                     for kind in kinds:
                         if float(pull[kind]) > 0:
                             developers[dev_id][kind] = 1
+                            kinds_flag[kind] = 1
                         else:
                             developers[dev_id][kind] = 0
-                        kinds_flag[kind] = 0
+                            kinds_flag[kind] = 0
                 
             
